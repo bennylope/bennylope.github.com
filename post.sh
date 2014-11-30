a185d27d84b7a36a4d76efe3fb413a6584967126
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2014-11-30 14:49:08 -0500

    Specify permalink in new post creation

--- post.sh
+++ post.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
- 
-# TODO add parameter for output path
-# TODO add usage string
-# TODO verify variables
- 
+
 # variables
 title="New Post"
 linkpost=
@@ -11,12 +7,11 @@ permalink=
 postdate=`date +%Y-%m-%d`
 posttime=`date +%H:%M:%S`
 publishdate="$postdate $posttime"
-tags=
 categories=
 openfile=false
- 
+
 # parameter handling
-while getopts "n:t:c:l:d:p:o" arg
+while getopts "n:l:d:p:o" arg
 do
     #echo "ARG is $arg"
     case "$arg" in
@@ -27,8 +22,6 @@ do
             posttime=`date -jf "%F %T" "$publishdate" +%H:%M:%S`;;
         p) permalink="$OPTARG";;
         o) openfile=true;;
-        t) tags="$OPTARG";;
-        c) categories="$OPTARG";;
         -) break;;
         \?) ;;
         *) echo "unhandled option $arg";;
@@ -36,44 +29,41 @@ do
             exit 1;;
     esac
 done
- 
+
 # create the file name
 lowercase=`echo $title | awk '{print tolower($0)}'`
 stripped=`echo ${lowercase// /-}`
 filename=$postdate-$stripped.md
 file=_drafts/$filename
- 
+
 # create the file and add content
-echo "---\n" >> $file
+echo "---" >> $file
 echo "title: \"$title\"" >> $file
- 
+
+if [ -z "$permalink" ]; then
+    echo "permalink: /$stripped/" >> $file
+else
+    echo "permalink: /$permalink/" >> $file
+fi
+
 if [ -z "$linkpost" ]; then
     echo "layout: post" >> $file
 else
     echo "layout: post-link" >> $file
     echo "post_link: $linkpost" >> $file
 fi
- 
+
 echo "published: false" >> $file
- 
-if [ -z "$categories" ]; then
-    echo "categories: Thoughts" >> $file
-else
-    echo "categroies: $categories" >> $file
-fi
- 
-echo "tags: $tags" >> $file
+
+
 echo "date: $publishdate" >> $file
-echo "\n---\n" >> $file >> $file
-echo "Content goes here." >> $file
- 
+echo "---\n" >> $file >> $file
+echo "Post content!" >> $file
+
 # open file if wanted
 if $openfile; then
     open $file
 fi
- 
+
 # done
-exit 0 
-fi
-    esac
-done
+exit 0
